fdf18f9fa96207e751f56d47f97fac77c9a27733
Leona_Lin <351397085@qq.com>
2021-06-26 16:23:10 +0800

    优惠倒计时

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -12,7 +12,7 @@
                 round
                 width="44px"
                 height="44px"
-                src="https://img.yzcdn.cn/vant/cat.jpeg"
+                :src="i.category_1_image"
                 />
                 <div class="category-name">{{i.category_1_name}}</div>
             </div>
@@ -20,11 +20,34 @@
         <v-devide/>
         <div class="discount-wrapper">
             <div class="discount-header">
-                <span>限时抢购 优惠直降</span>
-                
+                <span class="title">限时抢购 优惠直降</span>
+                <div class="discount-time">
+                    <van-count-down :time="time">
+                        <template #default="timeData">
+                            <span class="block">{{ timeData.hours }}</span>
+                            <span class="colon">:</span>
+                            <span class="block">{{ timeData.minutes }}</span>
+                            <span class="colon">:</span>
+                            <span class="block">{{ timeData.seconds }}</span>
+                        </template>
+                    </van-count-down>
+                </div>
             </div>
             <div class="good-wrapper">
                 <div class="good" v-for="i in [1,2,3]" :key="i">
+                    <div class="good-price">
+                        <span class="dollar">￥</span>
+                        <span class="price">55.8</span>
+                    </div>
+                    <div class="good-origin-price">
+                        <span class="dollar">￥</span>
+                        <span class="price">66.8</span>
+                    </div>
+                    <van-image
+                    width="82px"
+                    height="82px"
+                    src="https://img.yzcdn.cn/vant/cat.jpeg"
+                    />
                 </div>
             </div>
         </div>
@@ -62,16 +85,16 @@ export default {
   methods:{
   },
   data(){
-      const category1=[]
+      const category1=[];
       return{
-          category1
+          category1,
+          time:6*60*60*5000
       }
   },
   mounted(){
       getCategory1().then(
             value=>{
                 this.category1=value.category
-                console.log(this.category1)
             },
             reason=>{
                 console.log(reason)
@@ -109,16 +132,18 @@ export default {
                 .category-name{
                     color:#333333;
                     font-size: 12px;
+                    font-weight: 500;
                 }
             }
         }
         .discount-wrapper{
             width:100%;
             .discount-header{
+                position: relative;
                 height:44px;
                 width: 100%;
                 line-height: 44px;
-                span{
+                .title{
                     display: inline-block;
                     font-family: PingFangSC;
                     font-size: 16px;
@@ -127,17 +152,62 @@ export default {
                     letter-spacing: 0;
                     margin-left: 15px;
                 }
+                .discount-time{
+                    position: absolute;
+                    top:12px;
+                    left:160px;
+                    .colon {
+                        display: inline-block;
+                        margin: 0 4px;
+                        color: #111;
+                    }
+                    .block {
+                        display: inline-block;
+                        width: 16px;
+                        height: 16px;
+                        text-align: center;
+                        line-height: 16px;
+                        color: #fff;
+                        font-size: 12px;
+                        text-align: center;
+                        background-color: #111;
+                        border-radius: 2.13px;
+                    }
+                }
             }
             .good-wrapper{
                 display: flex;
                 justify-content: center;
                 width: 100%;
-                height: 129px;
+                height: 130px;
                 .good{
                     margin-right:1.83px;
                     width: 112.3px;
                     height:112.3px;
                     background: #F6F6F6;
+                    text-align: center;
+                    font-family: PingFangSC;
+                    .good-price{
+                        display: inline-block;
+                        margin-top:9px;
+                        margin-right: 4px;
+                        color: #FF9100;
+                        font-weight: 600;
+                        .dollar{
+                            font-size: 12px;
+                        }
+                        .price{
+                            font-size: 16px;
+                        }
+                    }
+                    .good-origin-price{
+                        margin-top:9px;
+                        display: inline-block;
+                        color: #999;
+                        text-decoration: line-through;
+                        font-size: 12px;
+                    }
+
                 }
                 .good:first-child{
                     border-radius: 4px 0 0 4px;
